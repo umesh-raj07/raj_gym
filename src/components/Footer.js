fb105a146477f52e7468dfeb74b6027fbc1d4cc6
Umesh Raj <134735341+umesh-raj07@users.noreply.github.com>
2023-12-14 22:00:55 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Prats from '../assets/images/Logo.png';
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Prats} alt="Prats" style={{ width: '200px', height: '200px' }} />
+    </Stack>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="11px" textAlign="center" pb="50px">No Pain No Gain</Typography>
+  </Box>
+);
+
+export default Footer;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '68d73d5fffmsh182d0668e01a6d4p1d8201jsn4687cff1b02f',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
